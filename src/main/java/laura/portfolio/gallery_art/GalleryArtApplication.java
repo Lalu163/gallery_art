544642ab767ce255fc1830196b5215556299f378
Laura Contreras <m.laura.cp89@gmail.com>
2022-05-05 19:39:45 +0200

    first commit

---
+++ src/main/java/laura/portfolio/gallery_art/GalleryArtApplication.java
@@ -0,0 +1,13 @@
+package laura.portfolio.gallery_art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryArtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalleryArtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/laura/portfolio/gallery_art/GalleryArtApplicationTests.java
@@ -0,0 +1,13 @@
+package laura.portfolio.gallery_art;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryArtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
